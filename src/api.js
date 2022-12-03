5833e8e1be3fc734f328b90d128c667396c551e7
IlyaRaduk <llya_radukrrr@mail.ru>
2022-12-03 20:25:20 +0300

    add baseURL axios

--- src/api.js
+++ src/api.js
@@ -1,28 +1,32 @@
 import axios from 'axios';
 
+const instance = axios.create({
+    baseURL:'http://192.168.100.3:3005/',
+})
+
 export let followAPI = (id) => {
-    return axios.post('http://192.168.100.3:3005/follow/' + id)
+    return instance.post('follow/' + id)
         .then((response) => {
             return (response.data)
         })
 }
 
 export let unfollowAPI = (id) => {
-    return axios.delete('http://192.168.100.3:3005/follow/' + id)
+    return instance.delete('follow/' + id)
         .then((response) => {
             return (response.data)
         })
 }
 
 export let getUsersAPI = (page) => {
-    return axios.get('http://192.168.100.3:3005/users/' + page)
+    return instance.get('users/' + page)
         .then(response => {
             return (response.data);
         })
 }
 
 export let getProfileAPI = (id) => {
-    return axios.get('http://192.168.100.3:3005/profile/' + id)
+    return instance.get('profile/' + id)
         .then((response) => {
             return (response.data);
         })
